4589da23becf03b0fa5fdf3b4ddbc4a56846c896
k-wall <kwall@apache.org>
2020-03-26 11:16:07 +0000

    Update oidckube.sh

--- oidckube.sh
+++ oidckube.sh
@@ -82,10 +82,10 @@ start_minikube() {
     --extra-config=apiserver.oidc-issuer-url=https://$KEYCLOAK_ADDRESS/auth/realms/$KEYCLOAK_AUTH_REALM \
     --extra-config=apiserver.oidc-client-id=$KEYCLOAK_CLIENT_ID \
     --extra-config=apiserver.oidc-username-claim=email \
-#    --extra-config=apiserver.oidc-username-prefix="oidc:" \
     --extra-config=apiserver.oidc-groups-claim=groups \
     --extra-config=apiserver.oidc-groups-prefix="oidc:" \
     --extra-config=apiserver.oidc-ca-file=/var/lib/minikube/certs/keycloak-ca.pem
+#    --extra-config=apiserver.oidc-username-prefix="oidc:" \
 }
 
 main() {
